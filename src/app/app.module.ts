8fbed1db1396fc12c6cb23cbbdac483b6d9c3ef2
jaime rey <jaimereycasado@gmail.com>
2023-05-24 18:27:27 +0200

    http services. el código cada vez da más problemas y se nota mucho más lo obsoleto que está el libro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientTestComponent } from './http-client-test/http-client-test.component';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HttpClientTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
